1283a3e06ec38209876344088183e538461cb2a3
Jessica Pope <jessica.pope.co@gmail.com>
2019-04-08 15:57:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 speak_to_grandma = 
-if phrase = phrase.uppercase
+if phrase == phrase.uppercase
   return "NO, NOT SINCE 1938!"
-else if phrase = "I LOVE YOU GRANDMA!"
+else if phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
 else
   return "HUH?! SPEAK UP, SONNY!"
